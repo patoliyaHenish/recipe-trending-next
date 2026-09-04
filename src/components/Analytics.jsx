1b3a7befe05f9207d3189dbee240b3c8fa0397c3
Patoliya Henish <henishpatoliya1508@gmail.com>
2026-09-04 21:33:55 +0530

    feat: add Google Analytics component with admin path filtering

--- src/components/Analytics.jsx
+++ src/components/Analytics.jsx
@@ -1,5 +1,6 @@
 "use client";
 import { usePathname } from 'next/navigation';
+import Script from 'next/script';
 
 export default function Analytics({ gaId }) {
   const pathname = usePathname();
@@ -11,17 +12,17 @@ export default function Analytics({ gaId }) {
 
   return (
     <>
-      <script async src={`https://www.googletagmanager.com/gtag/js?id=${gaId}`}></script>
-      <script dangerouslySetInnerHTML={{
-        __html: `
+      <Script src={`https://www.googletagmanager.com/gtag/js?id=${gaId}`} strategy="afterInteractive" />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
           gtag('config', '${gaId}', {
             debug_mode: true
           });
-        `
-      }} />
+        `}
+      </Script>
     </>
   );
 }
