44bd9fc95c1d0e6f24cbdb0fc5c8c620b5cd0a0b
Patoliya Henish <henishpatoliya1508@gmail.com>
2026-09-11 22:48:57 +0530

    chore(config): implement global cache-control headers

--- next.config.mjs
+++ next.config.mjs
@@ -13,6 +13,28 @@ const nextConfig = {
       },
     ],
   },
+  async headers() {
+    return [
+      {
+        source: '/((?!_next/static|_next/image|favicon.ico).*)',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'no-store, no-cache, must-revalidate, proxy-revalidate, max-age=0',
+          },
+        ],
+      },
+      {
+        source: '/_next/static/:path*',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'no-store, no-cache, must-revalidate, proxy-revalidate, max-age=0',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
